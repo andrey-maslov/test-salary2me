c35f773c89f7de51c36f30ffd7fa70f7c249f2ef
Andrey Maslov <andrei.maslau@gmail.com>
2021-01-08 14:36:48 +0300

    robotQuestion render fixed

--- src/components/common/media/robots/robot-question/RobotQuestion.tsx
+++ src/components/common/media/robots/robot-question/RobotQuestion.tsx
@@ -7,66 +7,66 @@ const RobotQuestion = () => {
         <div className={style.robot}>
             <span className={`${style.question} question-anim`}>?</span>
             <div className={`${style.arm} ${style.leftArm}`}>
-                <div className={style.shoulder}/>
+                <div className={style.shoulder} />
                 <div className={style.handWrap}>
-                    <div className={style.cubit}/>
-                    <div className={style.hand}/>
+                    <div className={style.cubit} />
+                    <div className={style.hand} />
                 </div>
             </div>
             <div className={style.center}>
                 <div className={style.top}>
                     <div className={style.antenna}>
-                        <div className={`${style.apex} antenna-anim`}/>
-                        <div className={style.vertical}/>
+                        <div className={`${style.apex} antenna-anim`} />
+                        <div className={style.vertical} />
                         <div className={`${style.curls} antenna-anim`}>
-                            <div/>
-                            <div/>
-                            <div/>
+                            <div />
+                            <div />
+                            <div />
                         </div>
                     </div>
                 </div>
                 <div className={style.head}>
                     <div className={style.pic}>
-                        <div/>
-                        <div/>
-                        <div/>
-                        <div/>
-                        <div/>
+                        <div />
+                        <div />
+                        <div />
+                        <div />
+                        <div />
                     </div>
-                    <div className={style.leftPart}/>
-                    <div className={style.rightPart}/>
+                    <div className={style.leftPart} />
+                    <div className={style.rightPart} />
                     <div className={style.eyes}>
-                        <div className={`${style.eye} eyes-anim`}/>
-                        <div className={`${style.eye} eyes-anim`}/>
+                        <div className={`${style.eye} eyes-anim`} />
+                        <div className={`${style.eye} eyes-anim`} />
                     </div>
-                    <div className={style.mouth}/>
+                    <div className={style.mouth} />
                 </div>
                 <div className={style.bottom}>
-                    <div className={style.body}/>
+                    <div className={style.body} />
                     <div className={style.legs}>
                         <div className={style.leg}>
-                            <div/>
-                            <div/>
-                            <div/>
-                            <div/>
-                            <div/>
+                            <div />
+                            <div />
+                            <div />
+                            <div />
+                            <div />
                         </div>
                         <div className={style.leg}>
-                            <div/>
-                            <div/>
-                            <div/>
-                            <div/>
-                            <div/>
+                            <div />
+                            <div />
+                            <div />
+                            <div />
+                            <div />
                         </div>
                     </div>
                 </div>
             </div>
             <div className={`${style.arm} ${style.rightArm}`}>
-                <div className={style.shoulder}/>
-                <div className={`right-hand-anim`}>
+                <div className={style.shoulder} />
+                <div className="right-hand-anim">
                     <div className={style.handWrap}>
-                        <div className={style.cubit}/>
-                        <div className={style.hand}/>
+                        <div className={style.cubit} />
+                        <div className={style.hand} />
                     </div>
                 </div>
             </div>
--- src/page-layouts/test/test-main/content/TestMainContent.tsx
+++ src/page-layouts/test/test-main/content/TestMainContent.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Link, withTranslation } from '@i18n'
 import { useMediaPredicate } from 'react-media-hook'
 import style from './main.module.scss'
@@ -5,13 +6,20 @@ import RobotQuestion from '../../../../components/common/media/robots/robot-ques
 
 const TestMainContent: React.FC<{ t }> = ({ t }) => {
     const isDesktop = useMediaPredicate('(min-width: 1360px)')
+    const [isRobotShown, setRobotShown] = useState(false)
+
+    useEffect(() => {
+        if (isDesktop) {
+            setRobotShown(true)
+        }
+    }, [isDesktop])
 
     return (
         <div className="pt-lg pb-lg">
             <div className="container">
                 <div className="row middle-xs">
                     <div className="col-xl-5">
-                        <div className="robot">{isDesktop && <RobotQuestion />}</div>
+                        <div className="robot">{isRobotShown && <RobotQuestion />}</div>
                     </div>
                     <div className="col-xl-7">
                         <div className={style.content}>
