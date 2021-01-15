dc9e3a442019a7b6b38aafc741417c086d3149fb
Andrey Maslov <andrei.maslau@gmail.com>
2021-01-15 17:32:17 +0300

    few fixes

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -19,14 +19,14 @@ function Layout({ children, t }) {
                 <meta property="og:url" content={HOST} />
                 <meta property="og:title" content={`${t('common:meta.title')} - ${SITE_TITLE}`} />
                 <meta property="og:description" content={t('common:meta.description')} />
-                <meta property="og:image" content={`${HOST}'/img/default.jpg`} />
+                <meta property="og:image" content={`${HOST}/img/default.jpg`} />
                 <meta property="og:locale" content={i18n.language} />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content={`${t('common:meta.title')} - ${SITE_TITLE}`} />
                 <meta name="twitter:description" content={t('common:meta.description')} />
                 <meta name="twitter:site" content={SITE_TITLE} />
                 <meta name="twitter:url" content={HOST} />
-                <meta name="twitter:image" content={`${HOST}'/img/default.jpg`} />
+                <meta name="twitter:image" content={`${HOST}/img/default.jpg`} />
                 <meta name="theme-color" content={accentColor} />
             </Head>
             <div className="app-wrapper">
--- src/page-layouts/test/test-result/Result.tsx
+++ src/page-layouts/test/test-result/Result.tsx
@@ -86,7 +86,7 @@ const Result: React.FC<ResultProps> = ({ t }) => {
     if (!resultForProfile || resultForProfile.length === 0) {
         return (
             <div className="flex-centered" style={{ padding: '3rem' }}>
-                <Link href="/test">
+                <Link href="/">
                     <a>
                         <h4>{t('test:errors.take_the_test')}</h4>
                     </a>
@@ -134,7 +134,7 @@ const Result: React.FC<ResultProps> = ({ t }) => {
     const secondaryOctantFraction: number | null = secondaryPsychoType
         ? getOctantFraction(sortedOctants[1], sortedOctants)
         : null
-    const accuracy = (mainOctantFraction / (mainOctantFraction + secondaryOctantFraction)) * 100
+    const accuracy = Math.round((mainOctantFraction / (mainOctantFraction + secondaryOctantFraction)) * 100)
 
     const fpTableHeader = [t('test:result_page.main_features'), t('test:result_page.revealed')]
 
